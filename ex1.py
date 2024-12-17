039dcf5715fe0d510ce31d250e635401d80d9491
3health26 <65422932+3health26@users.noreply.github.com>
2024-12-16 21:37:05 -0600

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,21 @@
+x=2
+y=3
+z= x + y 
+#print("ans is ", z)
+
+#print("hi how are you")
+
+# x,y,z = 12, 2, 3
+# print("x is ", x)
+# p,r,t=1000,2,3
+# i = (p*r*t)/100
+# print("simple int is ", i)
+h=90
+m=35
+sc=89
+ss=99
+e=80
+total=h+m+sc+ss+e
+per=total*100/500
+print("total is", total)
+print("per is", per, "%")
